f9df82677f9bc425b70558ddfd7df4b9ff6a0259
manthanank <manthan.ank46@gmail.com>
2021-11-25 10:19:46 +0530

    commit

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -8,9 +8,6 @@ import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 export class HeaderComponent implements OnInit {
   @Output() featureSelected = new EventEmitter<string>();
 
-  onSelect(feature:string){
-    this.featureSelected.emit(feature);
-  }
   constructor() { }
 
   ngOnInit(): void {
