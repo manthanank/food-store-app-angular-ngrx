8b9c9eaa2c1953bbacfa7bd675ecd781130b46d0
manthanank <manthan.ank46@gmail.com>
2021-11-27 17:08:42 +0530

    commit

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -35,8 +35,11 @@ export class RecipeEditComponent implements OnInit {
   onAddIngredient(){
     (<FormArray>this.recipeForm.get('ingredients')).push(
       new FormGroup({
-        'name': new FormControl(),
-        'amount': new FormControl()
+        'name': new FormControl(null, Validators.required),
+        'amount': new FormControl(null,[
+          Validators.required,
+                Validators.pattern(/^[1-9]+[0-9]*$/)
+        ])
       })
     )
   }
