dcd210683692f1fb84a35ca1a5dfb5d64d38c87f
manthanank <manthan.ank46@gmail.com>
2021-11-27 14:19:56 +0530

    commit

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormArray, FormControl, FormGroup } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Params } from '@angular/router';
 import { RecipeService } from '../recipe.service';
 
@@ -55,16 +55,19 @@ export class RecipeEditComponent implements OnInit {
         for(let  ingredient of recipe.ingredients){
           recipeIngredients.push(
             new FormGroup({
-              'name': new FormControl(ingredient.name),
-              'amount': new FormControl(ingredient.amount)
+              'name': new FormControl(ingredient.name, Validators.required),
+              'amount': new FormControl(ingredient.amount, [
+                Validators.required,
+                Validators.pattern(/^[1-9]+[0-9]*$/)
+              ])
             })
           );
         }
       }
     }
     this.recipeForm = new FormGroup({
-      'name': new FormControl(recipeName),
-      'imagePath': new FormControl(recipeImagepath),
+      'name': new FormControl(recipeName, Validators.required),
+      'imagePath': new FormControl(recipeImagepath,Validators.required),
       'description': new FormControl(recipeDrescription),
       'ingredients': recipeIngredients
     });
