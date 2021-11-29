ba12dd8b2012426e0254528339e643de5779e65c
manthanank <manthan.ank46@gmail.com>
2021-11-29 10:32:55 +0530

    commit

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -14,7 +14,8 @@ import { PlaceholderDirective } from '../shared/placeholder/placeholder.directiv
 
 @Component({
   selector: 'app-auth',
-  templateUrl: './auth.component.html'
+  templateUrl: './auth.component.html',
+  styleUrls: ['./auth.component.css']
 })
 export class AuthComponent implements OnDestroy {
   isLoginMode = true;
