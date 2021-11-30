e94023786562fe838a3a6ce7758d260e8ce8d239
manthanank <manthan.ank46@gmail.com>
2021-11-30 12:23:33 +0530

    Revert "commit"
    This reverts commit ba12dd8b2012426e0254528339e643de5779e65c.

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -26,7 +26,7 @@ export class AuthService {
   signup(email: string, password: string) {
     return this.http
       .post<AuthResponseData>(
-        'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyDKCuKJ8VRKaa5SNbUhh-SLPekZNgqfkc8',
+        'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyDb0xTaRAoxyCgvaDF3kk5VYOsTwB_3o7Y',
         {
           email: email,
           password: password,
@@ -49,7 +49,7 @@ export class AuthService {
   login(email: string, password: string) {
     return this.http
       .post<AuthResponseData>(
-        'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyDKCuKJ8VRKaa5SNbUhh-SLPekZNgqfkc8',
+        'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyDb0xTaRAoxyCgvaDF3kk5VYOsTwB_3o7Y',
         {
           email: email,
           password: password,
