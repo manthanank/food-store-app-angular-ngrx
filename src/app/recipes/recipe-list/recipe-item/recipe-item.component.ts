4c5e2236829e209e6bcc2ee608d05144d4258e3a
manthanank <manthan.ank46@gmail.com>
2021-12-01 01:31:00 +0530

    commit

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
+
 import { Recipe } from '../../recipe.model';
+
 @Component({
   selector: 'app-recipe-item',
   templateUrl: './recipe-item.component.html',
@@ -8,6 +10,7 @@ import { Recipe } from '../../recipe.model';
 export class RecipeItemComponent implements OnInit {
   @Input() recipe: Recipe;
   @Input() index: number;
-  ngOnInit(): void {
+
+  ngOnInit() {
   }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseAPIKey: 'AIzaSyDKCuKJ8VRKaa5SNbUhh-SLPekZNgqfkc8'
 };
 
 /*
